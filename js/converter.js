d03f3aa14d7183ee064a4b12723296646a5f5808
AlicjaWD <alice.wierzbicka777@protonmail.com>
2022-05-14 13:13:08 +0200

    changed name of variable "currency"

--- js/converter.js
+++ js/converter.js
@@ -1,26 +1,24 @@
 {
     const calculateResult = (amount, currency) => {
-        const EUR = 4.6862;
-        const USD = 4.2245;
-        const GBP = 5.5643;
-        const CHF = 4.5149;
+        const eur = 4.6862;
+        const usd = 4.2245;
+        const gbp = 5.5643;
+        const chf = 4.5149;
 
         switch (currency) {
             case "EUR":
-                return amount / EUR;
+                return amount / eur;
 
             case "USD":
-                return amount / USD;
+                return amount / usd;
 
             case "GBP":
-                return amount / GBP;
+                return amount / gbp;
 
             case "CHF":
-                return amount / CHF;
-
+                return amount / chf;
         }
     };
-
     const updateResultText = (amount, result, currency) => {
         const resultElement = document.querySelector(".js-result");
         resultElement.innerHTML = `${amount.toFixed(2)}PLN = <strong>${result.toFixed(2)}${currency}</strong>`;
@@ -29,16 +27,12 @@
         event.preventDefault();
         const amountElement = document.querySelector(".js-amount");
         const currencyElement = document.querySelector(".js-currency");
-
-
-
         const amount = +amountElement.value;
         const currency = currencyElement.value;
 
         const result = calculateResult(amount, currency);
 
         updateResultText(amount, result, currency);
-
     };
     const init = () => {
         const form = document.querySelector(".js-form");
